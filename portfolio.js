215e305ee3bf886b4305734e1a2c53e9fd09b648
bilalkatbi7 <bilalkatbi7@gmail.com>
2022-03-02 16:09:54 +0400

    re

---
+++ portfolio.js
@@ -0,0 +1,218 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Bilal Katbi",
+	title: "Hi all, I'm Bilal",
+	description:
+		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Python / Django / Flask and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+	resumeLink: "#",
+};
+
+export const openSource = {
+	githubUserName: "bilalkatbi7",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/bilalkatbi",
+	instagram: "https://www.instagram.com/bilalkatbi7",
+	twitter: "https://twitter.com/bilalkatbi7",
+	github: "https://github.com/katbi89",
+	linkedin: "https://www.linkedin.com/in/katbi89/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front End / User Interfaces for your web and mobile applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "TypeScript",
+			fontAwesomeClassname: "logos:typescript-icon",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "swift",
+			fontAwesomeClassname: "vscode-icons:file-type-swift",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	// {
+	// 	schoolName: "Harvard University",
+	// 	subHeader: "Master of Science in Computer Science",
+	// 	duration: "September 2017 - April 2019",
+	// 	desc: "Participated in the research of XXX and published 3 papers.",
+	// 	descBullets: [
+	// 		"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+	// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+	// 	],
+	// },
+	// {
+	// 	schoolName: "Google",
+	// 	subHeader: "Google IT Specialist",
+	// 	duration: "September 2017 - April 2019",
+	// 	desc: "Participated in the research of XXX and published 3 papers.",
+	// 	descBullets: [
+	// 		"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+	// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+	// 	],
+	// },
+	// {
+	// 	schoolName: "Stanford University",
+	// 	subHeader: "Bachelor of Science in Computer Science",
+	// 	duration: "September 2013 - April 2017",
+	// 	desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+	// 	descBullets: [
+	// 		"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
+	// 	],
+	// },
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "Aveneus",
+		companylogo: "/img/icons/common/avenues.png",
+		date: "June 2018 – Present",
+		desc: "Web Developer.",
+		// descBullets: [
+		// 	"Full-Stack",
+		// 	"Mobile Application",
+		// ],
+	},
+	{
+		role: "Front-End Developer",
+		company: "Smart Tech",
+		companylogo: "/img/icons/common/smartech.jpg",
+		date: "May 2017 – May 2018",
+		desc: "Front End Developer.",
+	},
+	{
+		role: "Web Training",
+		company: "Asia",
+		companylogo: "/img/icons/common/asia.jpg",
+		date: "Jan 2015 – Sep 2015",
+		desc: "Trainer For HTML - CSS - JavaScript.",
+	},
+];
+
+export const projects = [
+	{
+		name: "Asia Center",
+		desc: "Asia Training Center",
+		link: "http://asia-sy.com/",
+	},
+	{
+		name: "Smartech Center",
+		desc: "Smart Tech Training Center",
+		github: "https://smartech-sy.com/",
+	},
+	{
+		name: "Avenues",
+		desc: "Aveneues For Programming Solutions",
+		github: "https://avenues-solution.com/",
+		link: "https://avenues-solution.com/",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Ammar Alberzri",
+		feedback:
+			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Bilal Katbi has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose bilal web developer services for u.",
+	},
+	{
+		name: "Salim Karboj",
+		feedback:
+			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+	},
+];
